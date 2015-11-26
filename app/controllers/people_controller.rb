3daf7c2be1c12365d4d69e8fc8195ce1bb1e73a9
Josef Němeček <nemecjo2@fit.cvut.cz>
2015-11-26 12:40:53 +0100

    Person model added

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -10,6 +10,7 @@ class PeopleController < ApplicationController
   # GET /people/1
   # GET /people/1.json
   def show
+    @theses = Thesis.where(:person, @person)
   end
 
   # GET /people/new
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,6 @@
 class Person < ActiveRecord::Base
+  has_many :theses, dependent: :destroy
+  
   validates :username, presence: true
   validates :age, presence: true
 end
